a79171a9ef84c288337651b46e7e794371d3e114
Pablo Querales <pabloquerales20@gmail.com>
2025-05-18 10:32:14 +0200

    se corrigió importación del bp

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,6 +1,6 @@
 from .users import users_bp
 from .students import students_bp
-from .student_details import student_detail_bp
+from .student_details import student_details_bp
 
 # Esto se importa en app.py
-all_blueprints = [users_bp, students_bp, student_detail_bp]
+all_blueprints = [users_bp, students_bp, student_details_bp]
