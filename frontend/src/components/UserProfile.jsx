b6ba13b8a04ca93a134fb895a5147d8af42c01ef
Pablo Querales <pabloquerales20@gmail.com>
2025-06-06 01:25:46 +0200

    se creó la estructura base del perfil de usuario o de vista principal y se llamó el componente en el componente lobby

---
+++ frontend/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+import "../styles/UserProfile.css";
+export const UserProfile = () => {
+	return (
+		<>
+			<div className="user-header">
+				<h1>Prueba</h1>
+			</div>
+			<h2>de esfuerzo</h2>
+		</>
+	);
+};
--- frontend/src/pages/Lobby.jsx
+++ frontend/src/pages/Lobby.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import useStore from "../store";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import { Sidebar } from "../components/sidebar";
+import { UserProfile } from "../components/UserProfile";
 
 export const Lobby = () => {
-	const { backendUrl, setUserLogged, userLogged } = useStore();
+	const { backendUrl, setUserLogged } = useStore();
 	const navigate = useNavigate();
 	const [isLogged, setIsLogged] = useState(false);
 
@@ -44,8 +45,14 @@ export const Lobby = () => {
 
 	return (
 		<>
-			<div className="d-flex vh-100">{isLogged ? <Sidebar /> : <></>}</div>
-			<button onClick={() => console.log(userLogged)}>soy un botons</button>
+			{isLogged ? (
+				<div className="d-flex vh-100">
+					<Sidebar />
+					<UserProfile />
+				</div>
+			) : (
+				<></>
+			)}
 		</>
 	);
 };
