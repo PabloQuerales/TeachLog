f9161eda2a40ab9a1ccbcb832f1553eefc222946
Pablo Querales <pabloquerales20@gmail.com>
2025-06-07 00:32:23 +0200

    se delimitaron las dos partes del proyecto

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,11 +1,13 @@
 import "../styles/UserProfile.css";
 export const UserProfile = () => {
 	return (
-		<div className="container-fluid vw-100">
+		<div className="container p-0 d-flex flex-column vw-100 ">
 			<div className="user-header">
-				<h1>Prueba</h1>
+				<h1 className="m-0">Prueba</h1>
 			</div>
-			<h2>de esfuerzo</h2>
+			<div className="user-content">
+				<h2>de esfuerzo</h2>
+			</div>
 		</div>
 	);
 };
