7db3aaba8f3e497effb6e4f86bc7c5c33ce55a23
Pablo Querales <pabloquerales20@gmail.com>
2025-06-07 01:26:19 +0200

    se creó el container para la foto de perfil

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,9 +1,12 @@
 import "../styles/UserProfile.css";
+import useStore from "../store";
 export const UserProfile = () => {
+	const { userLogged } = useStore();
 	return (
 		<div className="container p-0 d-flex flex-column vw-100 ">
 			<div className="user-header">
 				<h1 className="m-0">Prueba</h1>
+				<img src={`https://api.dicebear.com/9.x/initials/svg?seed=${userLogged.name}`} className="user-avatar" />
 			</div>
 			<div className="user-content">
 				<h2>de esfuerzo</h2>
