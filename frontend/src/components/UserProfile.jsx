3c4cf179d7eab2a74af3b1d7e7228ec527a47938
Pablo Querales <pabloquerales20@gmail.com>
2025-06-07 13:47:33 +0200

    se terminó de crear la estructura base del home, agregando cards y distribución de espacios en los diseños

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -3,13 +3,42 @@ import useStore from "../store";
 export const UserProfile = () => {
 	const { userLogged } = useStore();
 	return (
-		<div className="container p-0 d-flex flex-column vw-100 ">
+		<div className="container p-0 d-flex flex-column vw-100 align-items-center">
 			<div className="user-header">
-				<h1 className="m-0">Prueba</h1>
 				<img src={`https://api.dicebear.com/9.x/initials/svg?seed=${userLogged.name}`} className="user-avatar" />
 			</div>
-			<div className="user-content">
-				<h2>de esfuerzo</h2>
+			<div className="user-content container">
+				<div className="row justify-content-around mt-5">
+					<div className="card text-bg-dark col-4 m-3">
+						<div className="card-body text-center">
+							<h5 className="card-title">Clases dictadas de MES EN CURSO AQUÍ</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+						</div>
+					</div>
+					<div className="card text-bg-dark m-3 col-4">
+						<div className="card-body text-center">
+							<h5 className="card-title">Clases totales</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+						</div>
+					</div>
+					<div className="card text-bg-dark m-3 col-4">
+						<div className="card-body text-center">
+							<h5 className="card-title">Dinero acumulado en MES EN CURSO AQUI</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+						</div>
+					</div>
+					<div className="card text-bg-dark m-3 col-4">
+						<div className="card-body text-center">
+							<h5 className="card-title">Total dinero generado</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+						</div>
+					</div>
+				</div>
+				<div className="d-flex justify-content-center">
+					<button type="button" class="btn btn-primary">
+						Base class
+					</button>
+				</div>
 			</div>
 		</div>
 	);
