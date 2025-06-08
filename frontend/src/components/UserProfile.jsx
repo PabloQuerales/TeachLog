d1849bb8cd417c8aa8b95c3c8ac5d85b427dcf39
Pablo Querales <pabloquerales20@gmail.com>
2025-06-08 13:02:37 +0200

    ajuste de algunos estilos en el home

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -9,25 +9,25 @@ export const UserProfile = () => {
 			</div>
 			<div className="user-content container">
 				<div className="row justify-content-around mt-5">
-					<div className="card text-bg-dark col-4 m-3">
+					<div className="card col-4 m-3">
 						<div className="card-body text-center">
 							<h5 className="card-title">Clases dictadas de MES EN CURSO AQUÍ</h5>
 							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
 						</div>
 					</div>
-					<div className="card text-bg-dark m-3 col-4">
+					<div className="card m-3 col-4">
 						<div className="card-body text-center">
 							<h5 className="card-title">Clases totales</h5>
 							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
 						</div>
 					</div>
-					<div className="card text-bg-dark m-3 col-4">
+					<div className="card m-3 col-4">
 						<div className="card-body text-center">
 							<h5 className="card-title">Dinero acumulado en MES EN CURSO AQUI</h5>
 							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
 						</div>
 					</div>
-					<div className="card text-bg-dark m-3 col-4">
+					<div className="card m-3 col-4">
 						<div className="card-body text-center">
 							<h5 className="card-title">Total dinero generado</h5>
 							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
@@ -35,8 +35,8 @@ export const UserProfile = () => {
 					</div>
 				</div>
 				<div className="d-flex justify-content-center">
-					<button type="button" class="btn btn-primary">
-						Base class
+					<button type="button" class="btn btn-secondary">
+						Agregar Registro
 					</button>
 				</div>
 			</div>
