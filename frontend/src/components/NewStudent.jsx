c82d939326e9ce5716018c59b96c784bded1720d
Pablo Querales <pabloquerales20@gmail.com>
2025-06-23 01:01:16 +0200

    se agregó el modal desplegable a la hora de presionar el boton de crear estudiante

--- frontend/src/components/NewStudent.jsx
+++ frontend/src/components/NewStudent.jsx
@@ -33,8 +33,31 @@ export const NewStudent = (props) => {
 		}
 	};
 	return (
-		<button className="btn btn-dark" onClick={newStudent}>
-			Crear Estudiante
-		</button>
+		<>
+			<button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#newStudenModal" onClick={newStudent}>
+				Crear Estudiante
+			</button>
+			<div class="modal fade" id="newStudenModal" tabindex="-1" aria-labelledby="newStudenModal" aria-hidden="true">
+				<div class="modal-dialog modal-dialog-centered">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h1 class="modal-title fs-5" id="exampleModalLabel">
+								Modal title
+							</h1>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div class="modal-body">...</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+								Close
+							</button>
+							<button type="button" class="btn btn-primary">
+								Save changes
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
