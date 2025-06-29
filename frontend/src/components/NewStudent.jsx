a74c6856540ed791c38b00e0eff34b4a19a78947
Pablo Querales <pabloquerales20@gmail.com>
2025-06-29 14:00:52 +0200

    se implementó la función completa de crear estudiantes nuevo

--- frontend/src/components/NewStudent.jsx
+++ frontend/src/components/NewStudent.jsx
@@ -1,19 +1,28 @@
+import { useState } from "react";
 import useStore from "../store";
 
 export const NewStudent = (props) => {
 	const { backendUrl, userLogged } = useStore();
+	const [inputValue, setInputValue] = useState({
+		name: "",
+		coin: "",
+		level: "",
+		price: "",
+		contact_name: "",
+		contact_phone: ""
+	});
 
 	const newStudent = async () => {
 		const myHeaders = new Headers();
 		myHeaders.append("Content-Type", "application/json");
 
 		const raw = JSON.stringify({
-			name: "Jose H",
-			price: "30",
-			coin: "EU",
-			contact_name: "Joanna",
-			contact_phone: "+31631261468",
-			level: "C1"
+			name: inputValue.name,
+			price: inputValue.price,
+			coin: inputValue.coin,
+			contact_name: inputValue.contact_name,
+			contact_phone: inputValue.contact_phone,
+			level: inputValue.level
 		});
 
 		const requestOptions = {
@@ -22,7 +31,7 @@ export const NewStudent = (props) => {
 			body: raw,
 			redirect: "follow"
 		};
-
+		console.log(inputValue);
 		try {
 			const response = await fetch(`${backendUrl}/new_student/${userLogged.id}`, requestOptions);
 			if (response.status == 200) {
@@ -31,60 +40,87 @@ export const NewStudent = (props) => {
 		} catch (error) {
 			console.error(error);
 		}
+		setInputValue({
+			name: "",
+			coin: "",
+			level: "",
+			price: "",
+			contact_name: "",
+			contact_phone: ""
+		});
+	};
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setInputValue({ ...inputValue, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		newStudent();
+		console.log("confirm");
 	};
 	return (
 		<>
-			<button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#newStudenModal" onClick={newStudent}>
+			<button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#newStudenModal">
 				Crear Estudiante
 			</button>
-			<div class="modal fade" id="newStudenModal" tabindex="-1" aria-labelledby="newStudenModal" aria-hidden="true">
-				<div class="modal-dialog modal-dialog-centered">
-					<div class="modal-content bg-dark text-white">
-						<div class="modal-header">
-							<h1 class="modal-title fs-5" id="newStudenModal">
+			<div className="modal fade" id="newStudenModal" tabIndex="-1" aria-labelledby="newStudenModal" aria-hidden="true">
+				<div className="modal-dialog modal-dialog-centered">
+					<div className="modal-content bg-dark text-white">
+						<div className="modal-header">
+							<h1 className="modal-title fs-5" id="newStudenModal">
 								Registro de Estudiantes
 							</h1>
-							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 						</div>
-						<div class="modal-body">
+						<div className="modal-body">
 							<form>
 								<div className="mb-3">
 									<label className="form-label">Nombre y Apellido</label>
-									<input type="text" className="form-control" />
+									<input type="text" className="form-control" name="name" value={inputValue.name} onChange={handleChange} />
 								</div>
-
 								<div className="row mb-3">
 									<div className="col">
 										<label className="form-label">Persona de Contacto</label>
-										<input type="text" className="form-control" />
+										<input type="text" className="form-control" name="contact_name" value={inputValue.contact_name} onChange={handleChange} />
 									</div>
 									<div className="col">
 										<label className="form-label">Número de Contacto</label>
-										<input type="text" className="form-control" />
+										<input type="text" className="form-control" name="contact_phone" value={inputValue.contact_phone} onChange={handleChange} />
 									</div>
 								</div>
-
-								<div className="row mb-3">
+								<div className="row mb-3 align-items-end">
 									<div className="col">
 										<label className="form-label">Nivel Actual del Estudiante</label>
-										<input type="text" className="form-control" />
+										<select className="form-select" aria-label="Level" name="level" required value={inputValue.level} onChange={handleChange}>
+											<option value="">Nivel</option>
+											<option value="A2">A2</option>
+											<option value="B1">B1</option>
+											<option value="B2">B2</option>
+											<option value="C1">C1</option>
+										</select>
 									</div>
 									<div className="col">
 										<label className="form-label">Precio de Clase X Hora</label>
-										<input type="text" className="form-control" />
+										<input type="number" className="form-control" name="price" value={inputValue.price} onChange={handleChange} />
 									</div>
 									<div className="col">
 										<label className="form-label">Moneda</label>
-										<input type="text" className="form-control" />
+										<select className="form-select" aria-label="Moneda" name="coin" required value={inputValue.coin} onChange={handleChange}>
+											<option value="">Moneda</option>
+											<option value="PEN">PEN</option>
+											<option value="EUR">EUR</option>
+											<option value="USD">USD</option>
+										</select>
 									</div>
 								</div>
 							</form>
 						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+						<div className="modal-footer">
+							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
 								Close
 							</button>
-							<button type="button" class="btn btn-primary">
+							<button type="button" className="btn btn-primary" onClick={handleSubmit} data-bs-dismiss="modal">
 								Save changes
 							</button>
 						</div>
