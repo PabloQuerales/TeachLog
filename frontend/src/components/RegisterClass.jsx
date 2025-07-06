01bad3372b21ca208720962e015a338d25f04e5f
Pablo Querales <pabloquerales20@gmail.com>
2025-07-06 13:15:57 +0200

    se agregó el boton para ver los detalles por estudiantes

--- frontend/src/components/RegisterClass.jsx
+++ frontend/src/components/RegisterClass.jsx
@@ -1,14 +1,20 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 export const RegisterClass = () => {
+	const path = useLocation();
+	useEffect(() => {
+		console.log(path.pathname);
+	}, []);
 	return (
 		<>
-			<button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#newStudenModal">
+			<button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#registerClass">
 				Añadir registro de estudiante
 			</button>
-			<div className="modal fade" id="newStudenModal" tabIndex="-1" aria-labelledby="newStudenModal" aria-hidden="true">
+			<div className="modal fade" id="registerClass" tabIndex="-1" aria-labelledby="registerClass" aria-hidden="true">
 				<div className="modal-dialog modal-dialog-centered">
 					<div className="modal-content bg-dark text-white">
 						<div className="modal-header">
-							<h1 className="modal-title fs-5" id="newStudenModal">
+							<h1 className="modal-title fs-5" id="registerClass">
 								Registro de Estudiantes
 							</h1>
 							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -16,8 +22,16 @@ export const RegisterClass = () => {
 						<div className="modal-body">
 							<form>
 								<div className="mb-3">
-									<label className="form-label">Nombre y Apellido</label>
-									{/* <input type="text" className="form-control" name="name" value={inputValue.name} onChange={handleChange} /> */}
+									<label className="form-label">Estudiante</label>
+									{path.pathname == "/students" ? (
+										<select className="form-select" aria-label="Level" name="level" required value="JUAN">
+											<option value="">Nivel</option>
+											<option value="A2">A2</option>
+											<option value="B1">B1</option>
+											<option value="B2">B2</option>
+											<option value="C1">C1</option>
+										</select>
+									) : null}
 								</div>
 								<div className="row mb-3">
 									<div className="col">
@@ -27,31 +41,6 @@ export const RegisterClass = () => {
 									<div className="col">
 										<label className="form-label">Número de Contacto</label>
 										{/* <input type="text" className="form-control" name="contact_phone" value={inputValue.contact_phone} onChange={handleChange} /> */}
-									</div>
-								</div>
-								<div className="row mb-3 align-items-end">
-									<div className="col">
-										<label className="form-label">Nivel Actual del Estudiante</label>
-										{/* <select className="form-select" aria-label="Level" name="level" required value={inputValue.level} onChange={handleChange}>
-											<option value="">Nivel</option>
-											<option value="A2">A2</option>
-											<option value="B1">B1</option>
-											<option value="B2">B2</option>
-											<option value="C1">C1</option>
-										</select> */}
-									</div>
-									<div className="col">
-										<label className="form-label">Precio de Clase X Hora</label>
-										{/* <input type="number" className="form-control" name="price" value={inputValue.price} onChange={handleChange} /> */}
-									</div>
-									<div className="col">
-										<label className="form-label">Moneda</label>
-										{/* <select className="form-select" aria-label="Moneda" name="coin" required value={inputValue.coin} onChange={handleChange}>
-											<option value="">Moneda</option>
-											<option value="PEN">PEN</option>
-											<option value="EUR">EUR</option>
-											<option value="USD">USD</option>
-										</select> */}
 									</div>
 								</div>
 							</form>
