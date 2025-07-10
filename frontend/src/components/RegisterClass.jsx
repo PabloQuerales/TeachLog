b1b88fdf0ac365f471d292de868c5745a910ad72
Pablo Querales <pabloquerales20@gmail.com>
2025-07-10 23:57:01 +0200

    se intaló la librería materialUI y se implementaron algunos cambios en el formulario para crear los registros

--- frontend/src/components/RegisterClass.jsx
+++ frontend/src/components/RegisterClass.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { TextField } from "@material-ui/core";
+
 export const RegisterClass = (props) => {
 	const [studentSelected, setStudentSelected] = useState("");
 	const path = useLocation();
@@ -7,7 +9,9 @@ export const RegisterClass = (props) => {
 	const handleClick = () => {
 		console.log(students.find((student) => student.name == "Julio Cesar"));
 	};
-
+	const handleChange = (e) => {
+		setStudentSelected(students.find((student) => student.name == e.target.value));
+	};
 	return (
 		<>
 			<button onClick={handleClick} type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#registerClass">
@@ -27,41 +31,50 @@ export const RegisterClass = (props) => {
 								<div className="mb-3">
 									<label className="form-label">Estudiante</label>
 									{path.pathname == "/students" ? (
-										<select
-											className="form-select"
-											aria-label="Name"
-											name="name"
-											required
-											onClick={() => setStudentSelected(students.find((student) => student.name == "Julio Cesar"))}>
+										<select className="form-select" aria-label="Name" name="name" required onChange={handleChange}>
 											<option value="">---Seleccionar Estudiante---</option>
 											{props.students.map((student) => {
-												return <option value={student.id}>{student.name}</option>;
+												return <option value={student.name}>{student.name}</option>;
 											})}
 										</select>
 									) : null}
 								</div>
-								<div className="row mb-3">
-									<div className="col">
-										<label className="form-label">Precio x Hora</label>
-										<input type="text" className="form-select text-center" name="contact_name" value={studentSelected.price} disabled />
+								{studentSelected ? (
+									<div className="row mb-3">
+										<div className="col">
+											<label className="form-label">Precio x Hora</label>
+											<input type="text" className="form-select text-center" name="contact_name" value={studentSelected.price} disabled />
+										</div>
+										<div className="col">
+											<label className="form-label">Moneda</label>
+											<input type="text" className="form-select text-center" name="contact_name" value={studentSelected.coin} disabled />
+										</div>
+										<div className="col">
+											<label className="form-label">Duración de la sesión</label>
+											<select aria-label="Time" name="time" required className="form-select">
+												<option value="">--Selecciona una Opción--</option>
+												<option value="0.5">30 min</option>
+												<option value="1">1 hora</option>
+												<option value="1.5">1 hora y 30 min</option>
+												<option value="2">2 horas</option>
+												<option value="2.5">2 horas y 30 min</option>
+												<option value="3">3 horas</option>
+											</select>
+										</div>
+										<div className="col">
+											<TextField
+												id="date"
+												label="Birthday"
+												type="date"
+												defaultValue="2017-05-24"
+												className={classes.textField}
+												InputLabelProps={{
+													shrink: true
+												}}
+											/>
+										</div>
 									</div>
-									<div className="col">
-										<label className="form-label">Moneda</label>
-										<input type="text" className="form-select text-center" name="contact_name" value={studentSelected.coin} disabled />
-									</div>
-									<div className="col">
-										<label className="form-label">Duración de la sesión</label>
-										<select aria-label="Time" name="time" required className="form-select">
-											<option value="">--Selecciona una Opción--</option>
-											<option value="0.5">30 min</option>
-											<option value="1">1 hora</option>
-											<option value="1.5">1 hora y 30 min</option>
-											<option value="2">2 horas</option>
-											<option value="2.5">2 horas y 30 min</option>
-											<option value="3">3 horas</option>
-										</select>
-									</div>
-								</div>
+								) : null}
 							</form>
 						</div>
 						<div className="modal-footer">
