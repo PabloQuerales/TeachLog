0a0bee70775890c3f63bc359be188571aaf20f15
Pablo Querales <pabloquerales20@gmail.com>
2025-07-11 00:24:40 +0200

    se terminó de crear la estructura del formulario añadiendo componentes de materialUI

--- frontend/src/components/RegisterClass.jsx
+++ frontend/src/components/RegisterClass.jsx
@@ -1,25 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { TextField } from "@material-ui/core";
+import TextField from "@mui/material/TextField";
 
 export const RegisterClass = (props) => {
 	const [studentSelected, setStudentSelected] = useState("");
+	const [currentDate, setCurrentDate] = useState("");
 	const path = useLocation();
 	const students = props.students;
+
 	const handleClick = () => {
-		console.log(students.find((student) => student.name == "Julio Cesar"));
+		console.log(students.find((student) => student.name === "Julio Cesar"));
 	};
+
 	const handleChange = (e) => {
-		setStudentSelected(students.find((student) => student.name == e.target.value));
+		setStudentSelected(students.find((student) => student.name === e.target.value));
 	};
+
+	useEffect(() => {
+		const now = new Date();
+		const formattedDate = now.toISOString().split("T")[0];
+		setCurrentDate(formattedDate);
+	}, []);
+
 	return (
 		<>
 			<button onClick={handleClick} type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#registerClass">
 				Añadir registro de estudiante
 			</button>
+
 			<div className="modal modal-lg fade" id="registerClass" tabIndex="-1" aria-labelledby="registerClass" aria-hidden="true">
 				<div className="modal-dialog modal-dialog-centered">
 					<div className="modal-content bg-dark text-white">
+						{" "}
+						{/* Mantienes los estilos de Bootstrap para el modal */}
 						<div className="modal-header">
 							<h1 className="modal-title fs-5" id="registerClass">
 								Registro de Estudiantes
@@ -30,50 +43,74 @@ export const RegisterClass = (props) => {
 							<form>
 								<div className="mb-3">
 									<label className="form-label">Estudiante</label>
-									{path.pathname == "/students" ? (
+									{path.pathname === "/students" ? (
 										<select className="form-select" aria-label="Name" name="name" required onChange={handleChange}>
 											<option value="">---Seleccionar Estudiante---</option>
 											{props.students.map((student) => {
-												return <option value={student.name}>{student.name}</option>;
+												return (
+													<option key={student.name} value={student.name}>
+														{student.name}
+													</option>
+												);
 											})}
 										</select>
 									) : null}
 								</div>
 								{studentSelected ? (
-									<div className="row mb-3">
-										<div className="col">
-											<label className="form-label">Precio x Hora</label>
-											<input type="text" className="form-select text-center" name="contact_name" value={studentSelected.price} disabled />
+									<>
+										<div className="row mb-3">
+											<div className="col">
+												<label className="form-label">Precio x Hora</label>
+												<input type="text" className="form-select text-center" name="contact_name" value={studentSelected.price} disabled />
+											</div>
+											<div className="col">
+												<label className="form-label">Moneda</label>
+												<input type="text" className="form-select text-center" name="contact_name" value={studentSelected.coin} disabled />
+											</div>
+											<div className="col">
+												<label className="form-label ">Duración de la sesión</label>
+												<select aria-label="Time" name="time" required className="form-select">
+													<option value="">--Selecciona--</option>
+													<option value="0.5">30 min</option>
+													<option value="1">1 hora</option>
+													<option value="1.5">1 hora y 30 min</option>
+													<option value="2">2 horas</option>
+													<option value="2.5">2 horas y 30 min</option>
+													<option value="3">3 horas</option>
+												</select>
+											</div>
 										</div>
-										<div className="col">
-											<label className="form-label">Moneda</label>
-											<input type="text" className="form-select text-center" name="contact_name" value={studentSelected.coin} disabled />
+										<div className="row">
+											<div className="col d-flex justify-content-center align-items-end">
+												<TextField
+													id="date"
+													label="Día"
+													type="date"
+													defaultValue={currentDate}
+													sx={{
+														"& .MuiInputLabel-root": {
+															color: "white"
+														},
+														"& .MuiInputBase-input": {
+															color: "white"
+														},
+														"& .MuiOutlinedInput-notchedOutline": {
+															borderColor: "white"
+														},
+														"& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+															borderColor: "white"
+														},
+														"&:hover .MuiOutlinedInput-notchedOutline": {
+															borderColor: "white"
+														},
+														"& .MuiInputAdornment-root .MuiSvgIcon-root": {
+															color: "white"
+														}
+													}}
+												/>
+											</div>
 										</div>
-										<div className="col">
-											<label className="form-label">Duración de la sesión</label>
-											<select aria-label="Time" name="time" required className="form-select">
-												<option value="">--Selecciona una Opción--</option>
-												<option value="0.5">30 min</option>
-												<option value="1">1 hora</option>
-												<option value="1.5">1 hora y 30 min</option>
-												<option value="2">2 horas</option>
-												<option value="2.5">2 horas y 30 min</option>
-												<option value="3">3 horas</option>
-											</select>
-										</div>
-										<div className="col">
-											<TextField
-												id="date"
-												label="Birthday"
-												type="date"
-												defaultValue="2017-05-24"
-												className={classes.textField}
-												InputLabelProps={{
-													shrink: true
-												}}
-											/>
-										</div>
-									</div>
+									</>
 								) : null}
 							</form>
 						</div>
@@ -82,8 +119,8 @@ export const RegisterClass = (props) => {
 								Close
 							</button>
 							{/* <button type="button" className="btn btn-primary" onClick={handleSubmit} data-bs-dismiss="modal">
-								Save changes
-							</button> */}
+                Save changes
+              </button> */}
 						</div>
 					</div>
 				</div>
