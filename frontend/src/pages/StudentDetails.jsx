2892b39db7ddb2ef3f3673d1b5fa7cc5f7496baa
Pablo Querales <pabloquerales20@gmail.com>
2025-07-24 17:48:15 +0200

    se creó un nuevo endpoint para hacer busquedas mas específicas por id de estudiante y se conectó con el front

--- frontend/src/pages/StudentDetails.jsx
+++ frontend/src/pages/StudentDetails.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import useStore from "../store";
 import { useParams } from "react-router-dom";
 
 export const StudentDetails = () => {
-	const { backendUrl, userLogged } = useStore();
+	const { backendUrl } = useStore();
 	const pathname = useParams();
-	const getStudent_Details = async () => {
+	const [student, setStudent] = useState([]);
+	const [studentDetails, setStudentDetails] = useState([]);
+
+	const getStudent = async () => {
+		const requestOptions = {
+			method: "GET",
+			redirect: "follow"
+		};
+
+		try {
+			const response = await fetch(`${backendUrl}/student/${pathname.id}`, requestOptions);
+			const result = await response.json();
+			setStudent(result);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	const getStudentDetails = async () => {
 		const requestOptions = {
 			method: "GET",
 			redirect: "follow"
@@ -14,16 +32,34 @@ export const StudentDetails = () => {
 			const response = await fetch(`${backendUrl}/student-details/student/${pathname.id}`, requestOptions);
 			const result = await response.json();
 			console.log(result);
+			setStudentDetails(result);
 		} catch (error) {
 			console.error(error);
 		}
 	};
-
+	useEffect(() => {
+		getStudent();
+		getStudentDetails();
+	}, []);
 	return (
 		<>
-			<button onClick={getStudent_Details}> PRUEBA GET</button>
-			<button onClick={() => console.log(pathname.id)}> PRUEBA PATH</button>
-			<h1>STUDENT ID: {userLogged.id}</h1>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col">
+						<h1>STUDENT NAME: {student.name}</h1>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col d-flex flex-column align-items-center">
+						{studentDetails.map((detail) => {
+							return <p key={detail.id}>date:{detail.date}</p>;
+						})}
+					</div>
+					<div className="col">
+						<p>Details: {studentDetails.length}</p>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
